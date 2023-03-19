4f6f8d03206deb3a8a1e173ef4ce02dbebd54bf1
kostenkoi24 <kostenko.i@gmail.com>
2023-03-19 20:06:14 +0200

    Add project files.

---
+++ SimpleApp/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿namespace SimpleApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public int Stock { get; set; }
+        public string Categories { get; set; }
+
+        public static string GetStocksStatus(int Stock)
+        {
+            if (Stock == 0)
+            {
+                return "немає в наявності";
+            }
+            if (Stock <= 5)
+            {
+                return "закінчується";
+            }
+            if (Stock > 5)
+            {
+                return "в наявності";
+            }
+            return "залишок не визначено";
+
+        }
+
+    }
+}
